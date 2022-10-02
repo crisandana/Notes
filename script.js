be65b1ec83a865e6f30b4a6693bb3bec72ecfcf7
crisandana <crisan.dana@outlook.com>
2022-10-03 00:54:59 +0200

    Step 6: add keyboard event listener

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const button = document.getElementById("add"); //"add" matches to the id from <button>(html)
 button.addEventListener("click", handleClick); // adding an event listener by using, button.addEventListener()
 // syntax addEventListener(type, handleFunction);
+const input = document.getElementById("text"); //"text" matches to the id from <input>(html)
+input.addEventListener("keydown", handleKeyDown);
 
-function handleClick(){
+function handleClick() {
+    add();
+}
+
+function handleKeyDown(event) {
+    if(event.key === "Enter") {
+        add();
+    }
+}
+
+function add(){
     const input = document.getElementById("text"); //"text" matches to the id from <input>(html)
     const note = input.value; // const note = findHTMLelement.value
-
     if(note) {
         const list = document.getElementById("list"); // Select the <ul> element using the id property
         const item = document.cretateElement("li"); // Create a new <item></item> element 
